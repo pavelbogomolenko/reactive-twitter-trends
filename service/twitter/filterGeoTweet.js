0b375d74ec73935051217fa27eec400c92851c28
Pavlo Bogomolenko <bpsource@gmail.com>
2016-04-29 21:42:54 +0200

    Fix coordinate order

--- service/twitter/filterGeoTweet.js
+++ service/twitter/filterGeoTweet.js
@@ -7,16 +7,19 @@ module.exports = (tweetObject, tweetObservable) => tweetObservable(tweetObject)
     .map(tweet => {
         let coordinates;
         if(_.get(tweet, 'coordinates.coordinates')) {
-            coordinates = _.get(tweet, 'coordinates.coordinates') || [0, 0];
+            coordinates = _.get(tweet, 'coordinates.coordinates');
         }
 
         if(_.get(tweet, 'place.bounding_box.coordinates[0][0]')) {
-            coordinates = _.get(tweet, 'place.bounding_box.coordinates[0][0]') || [0, 0];
+            coordinates = _.get(tweet, 'place.bounding_box.coordinates[0][0]');
         }
 
         return {
-            coordinates: coordinates.reverse(),
-            hashtag: tweetObject.name,
+            coordinates: coordinates,
+            hashtag: _.get(tweet, 'entities.hashtags[0].text'),
+            text: tweet.text,
+            id: tweet.id,
             socketId: tweetObject.socketId
         };
-    });
+    })
+    .filter(tweet => tweet.hashtag);
